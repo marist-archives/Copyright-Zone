2b992a949a93321e7b837f2c1ad353c49fe7bdce
Weon Yuan <weon.yuan@gmail.com>
2013-09-19 16:33:34 -0400

    Worked on side bar
    Utilizing jQuery UI library.

--- copyright.js
+++ copyright.js
@@ -45,6 +45,42 @@ function init() {
 	$('div#1slot').css('background-color', 'GREEN').append('<br/><a href="resources.html?iframe=true&width=55%&height=75%" class="link" rel="prettyphoto[iframes]">Resources</a>');
 	$('div#2slot').css('background-color', 'ORANGE').append('<br/><a href="msz.html?iframe=true&width=55%&height=75%" class="link" rel="prettyphoto[iframes]">Resources</a>');
 	$('div#3slot').css('background-color', '#b31b1b').append('<br/><a href="danger.html?iframe=true&width=55%&height=75%" class="link" rel="prettyphoto[iframes]">Help</a>');
+
+	// Tabs
+	$('ul.tabs').each(function(){
+		// For each set of tabs, we want to keep track of
+		// which tab is active and it's associated content
+		var $active, $content, $links = $(this).find('a');
+
+		// If the location.hash matches one of the links, use that as the active tab.
+		// If no match is found, use the first link as the initial active tab.
+		$active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+		$active.addClass('active');
+		$content = $($active.attr('href'));
+
+		// Hide the remaining content
+		$links.not($active).each(function () {
+			$($(this).attr('href')).hide();
+		});
+
+		// Bind the click event handler
+		$(this).on('click', 'a', function(e){
+			// Make the old tab inactive.
+			$active.removeClass('active');
+			$content.hide();
+
+			// Update the variables with the new link and content
+			$active = $(this);
+			$content = $($(this).attr('href'));
+
+			// Make the tab active.
+			$active.addClass('active');
+			$content.show();
+
+			// Prevent the anchor's default click action
+			e.preventDefault();
+		});
+	});
 }
 
 function handleCardDrop(event, ui) {
