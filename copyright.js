ae93f9afe844ae5ba2aa46f9a8ab117d1688e033
Weon Yuan <weon.yuan@gmail.com>
2013-09-25 12:22:04 -0400

    Tweaked Congratulations message

--- copyright.js
+++ copyright.js
@@ -6,15 +6,11 @@ var numbers = [1, 2, 3, 2, 1, 3, 3, 3, 1, 1];
 var resources = ['Create a Powerpoint with images downloaded from ARtstor and upload it into iLearn.', 'Embed a video from Youtube.', 'Upload into iLearn an article dowloaded from a database.', 'Photocopy or print a few chapters from a book.', 'Use a video for an assignment and would cite it properly.', 'Create a Prezi show for my class by making screen shots of some interesting photos I found on the web.', ' Upload into iLearn the article that I received in PDF format via ILL.', 'Download PDFs for 5 articles from Jstor and then upload those into iLearn.', 'Quote a paragraph from a book as the basis of an essay question on a test.', 'Embed a three minute clip of congressional testimony from C-SPAN.'];
 var reasons = ['This is fine because the Artstor’s “Terms of Use” specifically allow for this sort of classroom use.  You cannot share this Powerpoint openly on the web because it is not a password protected environment like iLearn.', 'Since the producer didn’t upload the video, it is likely that the post is a copyright violation. Search Fox Hunt limiting by video. Marist has over 6000 videos online.', 'This is a violation of copyright.  You are making a work by someone else widely available without consulting the copyright holder.  Copyright holders are due some consideration and compensation for sharing their work with others.  You will need to ask permission to use the work and often times you may need to pay for permission to use it.', 'This is what is called a one-time exception, it is an emergency, you cannot do the assignment without the book and due to forces beyond your control you do not have it.  The general expectation is that you will buy the book and not copy chapters as you need them.  It is not really a good idea to do this overall so it is on verge of being a red zone issue.', 'This is absolutely fine as you have cited it properly.', 'This may be a violation of copyright. You should always contact the site where you found the content. Alternatively, you could use Creative Commons which is a non profit organization that facilitates the sharing and use of materials through free legal tools.  [Link: <a href="http://www.creativecommons.org" target="_blank">Creative Commons</a>]', 'This is a violation of copyright. Alternatively, Find something similar in Library collection, either by the same author(s) or similar in content.  For example, you may find a chapter\ of a book in ebrary by the same author that makes small changes to the original article.  [Links: <a href="http://marist.summon.serialssolutions.com" target="_blank">Fox Hunt</a>, <a href="http://site.ebrary.com.online.library.marist.edu/lib/marist/home.action" target="_blank">ebrary</a>]', 'This is a copyright violation. Rather than uploading you should post links to those articles.', 'This is fine as it is only a small portion of the book.', 'C-SPAN’s copyright policy allows the use of video coverage of Federal Government events online for non commercial purposes with attributions.'];
 function init() {
-
+	$('div#info').empty();
+	$('div#info').css('background-color', '#ffffff');
+	$('div.wideBox div#inst').hide();
 	// Hide the success message
-	/*$('#successMessage').hide();
-	$('#successMessage').css({
-		left : '580px',
-		top : '250px',
-		width : 0,
-		height : 0
-	});*/
+	$('#successMessage').hide();
 
 	// Reset the game
 	correctCards = 0;
@@ -124,17 +120,9 @@ function handleCardDrop(event, ui) {
 	// If all the cards have been placed correctly then display a message
 	// and reset the cards for another go
 
-	if (correctCards == 1) {
-		/*$('#successMessage').show();
-		$('#successMessage').animate({
-			left : '580px',
-			top : '500px',
-			width : '400px',
-			height : '100px',
-			opacity : 1
-		});*/
-		$('#cardPile div').remove();
-		$('#cardPile').append('<p id="successMessage">Congratulations!</p><p>You have finished the game by successfully placing all the cards in their zones.</p><p>Click <a href="#" onclick="init();">here</a> to play again.</p>');
+	if (correctCards == 10) {
+		$('#successMessage').show();
+		$('#successMessage').animate();
 		$("html, body").animate({
 	        scrollTop: 0
 	    }, 500);
